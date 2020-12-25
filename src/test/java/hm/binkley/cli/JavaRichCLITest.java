b7d485b9f3934982861c37605534055dfdff1944
Brian Oxley <boxley@thoughtworks.com>
2020-12-25 04:48:45 -0600

    More Java testing

--- src/test/java/hm/binkley/cli/JavaRichCLITest.java
+++ src/test/java/hm/binkley/cli/JavaRichCLITest.java
@@ -1,6 +1,11 @@
 package hm.binkley.cli;
 
+import org.jline.reader.Completer;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.TerminalBuilder;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
@@ -9,10 +14,6 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
  * test library functionality directly 2. Only use mocks as a last resort
  */
 class JavaRichCLITest {
-    private static RichCLI<TestOptions> testRichCLI(final String... args) {
-        return new RichCLI<>(new TestOptions(), args);
-    }
-
     @Test
     void shouldConstructWithNoArgs() {
         try (var cli = testRichCLI()) {
@@ -29,5 +30,34 @@ class JavaRichCLITest {
             assertArrayEquals(expected, cli.getOptions().getArgs(),
                     "Wrong arguments");
         }
+    }
+
+    @Test
+    void shouldConstructWithCustomEverything() throws IOException {
+        final var options = new TestOptions();
+        final Completer completer = (reader, line, candidates) -> {
+        };
+        final var terminal = TerminalBuilder.builder()
+                .dumb(true)
+                .build();
+        final var lineReader = LineReaderBuilder.builder()
+                .appName(terminal.getName())
+                .completer(completer)
+                .terminal(terminal)
+                .build();
+
+        // Test that:
+        // 1. Compiles
+        // 2. Does not throw
+        new RichCLI<>(
+                options,
+                terminal,
+                completer,
+                lineReader,
+                "-d", "arg1", "arg2");
+    }
+
+    private static RichCLI<TestOptions> testRichCLI(final String... args) {
+        return new RichCLI<>(new TestOptions(), args);
     }
 }
