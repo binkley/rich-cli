e17b4b163b9695abf71d64ef647909209653b572
Brian Oxley <boxley@thoughtworks.com>
2020-12-25 05:17:56 -0600

    More non-working tests

--- src/test/kotlin/hm/binkley/cli/DemoKtTest.kt
+++ src/test/kotlin/hm/binkley/cli/DemoKtTest.kt
@@ -8,12 +8,23 @@ import org.junit.jupiter.api.Test
 internal class DemoKtTest {
     @Disabled("TODO: Find out where STDOUT is going; this works for other tests than main")
     @Test
-    fun `should demo`() {
+    fun `should demo with defaults`() {
         val out = tapSystemOutNormalized {
-            main("arg1", "arg2")
+            main()
         }.trim()
 
         out shouldContain "DEBUG? -> false"
-        out shouldContain "ARGS? -> [args1, arg2]"
+        out shouldContain "ARGS? -> []"
+    }
+
+    @Disabled("TODO: Find out where STDOUT is going; this works for other tests than main")
+    @Test
+    fun `should demo with command line`() {
+        val out = tapSystemOutNormalized {
+            main("-d", "arg1", "arg2")
+        }.trim()
+
+        out shouldContain "DEBUG? -> true"
+        out shouldContain "ARGS? -> [arg1, arg2]"
     }
 }
